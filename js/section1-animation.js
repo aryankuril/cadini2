7a76e71ebfcde3404c07782cc34228a8d2e0393f
Aryan Kuril <160471508+aryankuril@users.noreply.github.com>
2025-06-08 19:23:12 +0530

    project added

--- js/section1-animation.js
+++ js/section1-animation.js
@@ -15,35 +15,13 @@ document.querySelector('.scroll-container').addEventListener('click', () => {
 
 function positionBottleCapOnBottle() {
   const bottleWithoutCap = document.getElementById("bottle-without-cap");
-  const bottleCap = document.getElementById("bottle-cap");
   const section1Title = document.getElementById("section-1-title");
-  const section1Button = document.getElementById("section-1-button");
+
 
   bottleWithoutCap.style.bottom = isMobile() ? "15%" : "5%";
 
-  if (bottleWithoutCap && bottleCap && section1Title) {
-    const bottleRect = bottleWithoutCap.getBoundingClientRect();
-    const bottleCapRect = bottleCap.getBoundingClientRect();
-
-    const bottleCapHeight = bottleCapRect.height;
-
-    const bottleTop = bottleRect.top;
-
-    bottleCap.style.bottom = `calc(100% - ${bottleTop}px - ${bottleCapHeight}px + 10px)`;
-    bottleCap.style.top = "auto";
-    section1Title.style.bottom = `calc(100% - ${bottleTop}px - ${bottleCapHeight}px + ${
-      isMobile() ? "100px" : "120px"
-    })`;
-    section1Title.style.top = "auto";
-
-    section1Button.style.bottom = `calc(100% - ${bottleTop}px - ${bottleCapHeight}px + ${
-      isMobile() ? "70px" : "100px"
-    })`;
-    section1Button.style.top = "auto";
-  }
 }
 
-window.addEventListener("load", positionBottleCapOnBottle);
 
 // Debounce function to limit how often resize handler is called
 function debounce(func, wait) {
@@ -73,12 +51,6 @@ const handleSafeResize = debounce(() => {
 
 window.addEventListener("resize", handleSafeResize);
 
-window.addEventListener("orientationchange", () => {
-  setTimeout(() => {
-    positionBottleCapOnBottle();
-    ScrollTrigger.refresh();
-  }, 300);
-});
 
 const tl1 = gsap.timeline({
   scrollTrigger: {
@@ -97,15 +69,7 @@ const tl1 = gsap.timeline({
   },
 });
 
-// Starting animation
-tl1.to(
-  "#bottle-cap",
-  {
-    yPercent: isMobile() ? -180 : -150,
-    duration: 0.3,
-  },
-  "tl1"
-);
+
 
 tl1.to(
   "#section-1-title",
@@ -137,22 +101,8 @@ tl1.to(
   "tl1"
 );
 
-tl1.to(
-  "#section-1-button",
-  {
-    opacity: 1,
-    scale: 1,
-    duration: 0.3,
-  },
-  "tl1"
-);
 
-tl1.to(
-  {},
-  {
-    duration: 0.5,
-  }
-);
+
 
 tl1.to(
   "#section-1",
@@ -184,14 +134,7 @@ tl1.to(
   "tl1.1"
 );
 
-tl1.to(
-  "#bottle-cap",
-  {
-    yPercent: 0,
-    duration: 0.3,
-  },
-  "tl1.1"
-);
+
 
 tl1.to(
   "#section-1-title",
@@ -204,16 +147,7 @@ tl1.to(
   "tl1.1"
 );
 
-tl1.to(
-  "#section-1-button",
-  {
-    opacity: 0,
-    scale: 0,
-    duration: 0.2,
-    ease: "power2.inOut",
-  },
-  "tl1.1"
-);
+
 
 tl1.to(
   "#bottle-without-cap",
